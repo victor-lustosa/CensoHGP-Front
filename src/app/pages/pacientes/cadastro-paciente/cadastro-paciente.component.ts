9dbd72b8b71359df8821ecd0733b2d7bc4a91555
victor152220 <victor.lustosa1522@live.com>
2021-02-17 12:08:28 -0300

    adicionando i18n ao modal de nascimento

--- src/app/pages/pacientes/cadastro-paciente/cadastro-paciente.component.ts
+++ src/app/pages/pacientes/cadastro-paciente/cadastro-paciente.component.ts
@@ -1,19 +1,36 @@
-import { Component, OnInit, Input, EventEmitter, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, ViewChild, Injectable } from '@angular/core';
 import { FormArray, FormGroup } from '@angular/forms';
-import { NgbActiveModal,  NgbDatepicker,  NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal,  NgbDatepicker,  NgbDatepickerI18n,  NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import { Departamento } from '../../departamentos/model/departamento';
 import { Precaucao } from '../../precaucoes/model/precaucao';
 import { DepartamentoService } from '../../departamentos/service';
 import { PacienteService } from '../service/paciente.service';
-import { PrecaucaoService } from '../../precaucoes/service/precaucao.service';
+
+
+
+const I18N_VALUES = {
+  'pt': {
+    weekdays: ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom'],
+    months: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dec'],
+  }
+  // other languages you would support
+};
+@Injectable()
+export class I18n {
+  language = 'pt';
+}
+
+
 
 @Component({
   selector: 'app-cadastro-paciente',
   templateUrl: './cadastro-paciente.component.html',
-  styleUrls: ['./cadastro-paciente.component.scss']
-})
+  styleUrls: ['./cadastro-paciente.component.scss'],
+  // providers: [I18n, {provide: NgbDatepickerI18n, useClass: CustomDatepickerI18n}]
 
+})
 export class CadastroPacienteComponent implements OnInit {
+
   @Input() public formulario: FormGroup;
   @Input() public listaPrecaucoes:Precaucao[]=[];
   errors: String[];
@@ -87,3 +104,23 @@ export class CadastroPacienteComponent implements OnInit {
             }
           }
         }
+        // @Injectable()
+        // export class CustomDatepickerI18n extends CadastroPacienteComponent {
+        //
+        //   constructor(private _i18n: I18n) {
+        //     super();
+        //   }
+        //   getWeekdayShortName(weekday: number): string {
+        //     return I18N_VALUES[this._i18n.language].weekdays[weekday - 1];
+        //   }
+        //   getMonthShortName(month: number): string {
+        //     return I18N_VALUES[this._i18n.language].months[month - 1];
+        //   }
+        //   getMonthFullName(month: number): string {
+        //     return this.getMonthShortName(month);
+        //   }
+        //
+        //   getDayAriaLabel(date: NgbDateStruct): string {
+        //     return `${date.day}-${date.month}-${date.year}`;
+        //   }
+        //   }
