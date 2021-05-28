b61cf189da5ed7b7994f40607e59b5292cc68aff
Victor Lustosa <victor.lustosa1522@live.com>
2021-05-28 10:46:56 -0300

    colocando gerenciamento de rotas filhas no proprio authGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { AuthGuard } from './pages/auth/guards/auth.guard';
-import { EnferGuard } from './pages/auth/guards/enfer.guard';
 import { AdminComponent } from './theme/layout/admin/admin.component';
 import {AuthComponent} from './theme/layout/auth/auth.component';
 
@@ -13,11 +12,11 @@ const routes: Routes = [
 }
 , { path: '',  component: AdminComponent, children: [
     { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(module => module.DashboardModule), canActivate : [AuthGuard]},
-    { path: 'usuarios', loadChildren: () => import('./pages/usuarios/usuarios.module').then(module => module.UsuariosModule), canActivate : [AuthGuard], canActivateChild: [EnferGuard]},
-    { path: 'precaucoes', loadChildren: () => import('./pages/precaucoes/precaucoes.module').then(module => module.PrecaucoesModule), canActivate : [AuthGuard], canActivateChild: [EnferGuard]},
-    { path: 'departamentos', loadChildren: () => import('./pages/departamentos/departamentos.module').then(module => module.DepartamentosModule), canActivate : [AuthGuard], canActivateChild: [EnferGuard]},
+    { path: 'usuarios', loadChildren: () => import('./pages/usuarios/usuarios.module').then(module => module.UsuariosModule), canActivate : [AuthGuard], canActivateChild: [AuthGuard]},
+    { path: 'precaucoes', loadChildren: () => import('./pages/precaucoes/precaucoes.module').then(module => module.PrecaucoesModule), canActivate : [AuthGuard], canActivateChild: [AuthGuard]},
+    { path: 'departamentos', loadChildren: () => import('./pages/departamentos/departamentos.module').then(module => module.DepartamentosModule), canActivate : [AuthGuard], canActivateChild: [AuthGuard]},
     { path: 'pacientes', loadChildren: () => import('./pages/pacientes/pacientes.module').then(module => module.PacientesModule), canActivate : [AuthGuard]},
-    { path: 'checklists', loadChildren: () => import('./pages/checklists/checklists.module').then(module => module.ChecklistsModule), canActivate : [AuthGuard], canActivateChild: [EnferGuard]}
+    { path: 'checklists', loadChildren: () => import('./pages/checklists/checklists.module').then(module => module.ChecklistsModule), canActivate : [AuthGuard], canActivateChild: [AuthGuard]}
 ]
 }
 ];
