5381e09102d8618303ba27f366df5134188f08de
Iury Felipe <iurycomputacao@gmail.com>
2021-07-21 11:06:48 -0300

    Criando o resolver de transferencia

---
+++ src/app/pages/pacientes/guards/transferencia.resolver.ts
@@ -0,0 +1,20 @@
+import { PacienteService } from './../service/paciente.service';
+import { Injectable } from '@angular/core';
+import { EMPTY, Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { catchError } from 'rxjs/internal/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TransferenciaResolver implements Resolve<any> {
+  constructor(private pacienteService: PacienteService) { }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
+    return this.pacienteService.getAllTransferenciaPaciente(route.params['id']).pipe(
+      catchError((error) => {
+       return EMPTY;
+     }));
+  }
+}
